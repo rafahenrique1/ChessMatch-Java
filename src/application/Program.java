ce6aad272e7910d8fc853b5772aac329c9ba959a
Rafael Henrique <rafahenriquee13@yahoo.com.br>
2021-11-02 14:17:54 -0300

    Check logic

--- src/application/Program.java
+++ src/application/Program.java
@@ -35,8 +35,8 @@ public class Program {
 				ChessPosition target = UI.readChessPosition(sc);
 
 				ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-				
-				if(capturedPiece != null) {
+
+				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
 			} catch (ChessException e) {
